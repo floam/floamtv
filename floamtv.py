2834f82481de312fae74bb0e60ba2d19a39e1bae
Aaron Gyes <floam@sh.nu>
2008-03-30 21:30:29 -0700

    I'm gonna release tonight.

--- floamtv.py
+++ floamtv.py
@@ -3,6 +3,12 @@
 # floamtv.py (Copyright 2008 Aaron Gyes)
 # distributed under the GPLv3. See LICENSE
 
+# Remaining bugs:
+#  - Two episdes on probation, they'll both get enqueued as soon as the first.
+#  - Implement attributes for fucks sake!
+#  - Logging.
+
+
 from __future__ import with_statement
 import re, os, csv, yaml, time, sys, errno, atexit
 from cStringIO import StringIO
@@ -19,16 +25,8 @@ dbpath = os.path.expanduser('~/.floamtvdb2')
 configpath = os.path.expanduser('~/.floamtvconfig2')
 pidfile = os.path.expanduser('~/.floamtvpid')
 
-tr = re.compile(r"tvrage\.com/.*/([\d]{4,8})")
-
-try:
-   with open(configpath, 'r') as configuration:
-      config = yaml.load(configuration)
-except IOError:
-   print 'You need to set up a config file first. See the docs.'
-   sys.exit()
-
 tasks = {}
+tr = re.compile(r"tvrage\.com/.*/([\d]{4,8})")
 
 class Collection(yaml.YAMLObject, xmlrpc.XMLRPC):
    """
@@ -206,31 +204,43 @@ class Collection(yaml.YAMLObject, xmlrpc.XMLRPC):
    xmlrpc_rewant = rewant
 
 class Show(yaml.YAMLObject):
-   "A Show represents a television show, which has Episodes as children."
+   """
+   A Show represents a television show, which has Episodes as children. Takes
+   only one argument, the show title. Does not automatically populate itself,
+   you will need to .update() it with information.
+   """
+   
    yaml_tag = '!Show'
    def __init__(self, title):
       self.episodes = []
       self.title = title
    
    def _add_episode(self, infodict):
-      "Given an infodict, create a new Episode in self.episodes"
-      ep = Episode(infodict)
-      if ep.tvrageid:
-         self.episodes.append(ep)
-         print "New episode: %s" % ep
+      "Given a dict with TVRage info, create a new Episode in self.episodes"
+      
+      if infodict['title']:
+         ep = Episode(**infodict)
+         if ep.tvrageid:
+            self.episodes.append(ep)
+            print "New episode: %s" % ep
    
    def add(self, episode):
       """
       Given an episode-number string in the format used by TVRage, ('3x04' being
       season 3, episode 4), look up further information on this show and add it
-      to self.episodes.  
+      to self.episodes. If we already know about the show we won't do anything.
       """
+      
       if episode and episode not in (e.number for e in self.episodes):
          newepisode = tvrage_info(self.title, episode)
          newepisode.addCallback(self._add_episode)
          return newepisode
    
    def update(self, rageinfo):
+      """
+      Given a dict with TVRage information on this show, try to add both the
+      latest episode and the upcoming episode to this Show.
+      """
       rcnt = filter(bool, [rageinfo['latest'], rageinfo['next']])
       
       cbs = [self.add(ep) for ep in rcnt]
@@ -244,17 +254,32 @@ class Show(yaml.YAMLObject):
    
 
 class Episode(yaml.YAMLObject):
+   """
+   Represents a television episode, takes a bunch of arguments:
+   
+    wecallit: Show's title, however not the real name, but what we searched for.
+    title:    Episode title.
+    tvrageid: Integer ID number used by TVRage to identify an episode.
+    airs:     datetime.datetime() or None if unknown airtime.
+    number:   Something like '3x05', season 3 episode 5.
+   """
+   
    yaml_tag = '!Episode'
-   def __init__(self, info):
-      self.show = info['wecallit']
-      self.number = info['number']
-      self.title = info['title']
-      self.tvrageid = info['tvrageid']
-      self.airs = info['airs']
+   def __init__(self, wecallit, number, title, tvrageid, airs, *a, **kw):
+      self.show = wecallit
+      self.number = number
+      self.title = title
+      self.tvrageid = tvrageid
+      self.airs = airs
       self.newzbinid = None
       self.wanted = True
    
    def enqueue(self, allowiffy=False):
+      """
+      Enqueue episodes that have a Newzbin ID resolved with Hellanzb. If
+      allowiffy is True, don't exclude shows that are on probation.
+      """
+      
       if self.newzbinid and self.wanted != 'later' or allowiffy:
          try:
             hella = ServerProxy("http://hellanzb:%s@%s:8760"
@@ -268,15 +293,22 @@ class Episode(yaml.YAMLObject):
          self.wanted = False
    
    def was_fake(self, sure=True):
-      if sure:
-         if self.wanted != 'later':
+      """
+      Call this if we find out a newzbinid for a show is actually pointing at a
+      fake post. It'll be put to the same state it was at before we found a
+      newzbinid for it. Call with sure = False if the post is suspect prior to
+      downloading -- this will put it on 'probation' for a couple hours and only
+      download it if it isn't deleted from Newzbin in the interim.
+      """
+      if self.wanted != 'later':
+         if sure:
             print "%s was fake. Requeueing." % self
-         self.newzbinid = None
-         self.wanted = True
-      elif self.wanted != 'later':
-         print "%s is too early. Will confirm in a couple hours." % self
-         self.wanted = 'later'
-         reactor.callLater(60*60*2, self.enqueue, True)
+            self.newzbinid = None
+            self.wanted = True
+         elif self.wanted:
+            print "%s is too early. Will confirm in a couple hours." % self
+            self.wanted = 'later'
+            reactor.callLater(60*60*2, self.enqueue, True)
    
    def __repr__(self):
       return "<Episode %s - %s - %s>" \
@@ -285,15 +317,45 @@ class Episode(yaml.YAMLObject):
    def __str__(self):
       return "%s - %s - %s, [%s]" % (self.show, self.number, self.title,
                                      humanize(self.tvrageid))
-   
 
 
-def tvrage_info(show_name, episode):
-   episode = episode or ''
-   u = urlencode({'show': show_name, 'ep': episode})
-   info = getPage("http://tvrage.com/quickinfo.php?%s" % u)
-   info.addCallback(parse_tvrage, show_name)
-   return info
+def am_server():
+   "Returns True if there isn't another running instance of floamtv."
+   pid = check_pid()
+   if pid is None or pid == os.getpid():
+      return True
+
+def at_exit(showset):
+   print "Cleaning shit up."
+   os.unlink(pidfile)
+   showset.save()
+
+def check_pid():
+   if os.path.exists(pidfile):
+      with open(pidfile) as f:
+         pid = f.read()
+      
+      try:
+         os.kill(int(pid), 0)
+      except os.error, err:
+         if err.errno == errno.ESRCH:
+            os.unlink(pidfile)
+      else:
+         return int(pid)
+
+def humanize(q):
+   'Converts number to a base33 format, 0-9,a-z except i,l,o (look like digits)'
+   if q < 0: raise ValueError, 'must supply a positive integer'
+   letters = '0123456789abcdefghjkmnpqrstuvwxyz'
+   converted = []
+   while q != 0:
+       q, r = divmod(q, 33)
+       converted.insert(0, letters[r])
+   return ''.join(converted)
+
+def load():
+   with open(dbpath, 'r') as savefile:
+      return yaml.load(savefile)
 
 def parse_tvrage(text, wecallit):
    rage = clean = defaultdict(lambda: None)
@@ -322,6 +384,25 @@ def parse_tvrage(text, wecallit):
       clean['airs'] = None
    
    return clean
+
+def relative_datetime(date):
+   # taken from <http://odondo.wordpress.com/2007/07/05/>, thanks!
+   if date:
+      diff = date.date() - dt.now().date()
+
+      if diff.days == 0:
+         return "airs %s today" % date.strftime("%I:%M %p")
+      elif diff.days == -1:
+         return "aired %s yesterday" % date.strftime("%I:%M %p")
+      elif diff.days < -1:
+         return "aired on %s" % date.strftime("%m/%d/%Y")
+      elif diff.days == 1:
+         return "airs %s tomorrow" % date.strftime("%I:%M %p")
+      elif diff.days > -7:
+         return "airs %s" % date.strftime("%I:%M %p %A")
+      else:
+         return "airs on %s" % date.strftime("%m/%d/%Y")
+   else: return 'Unknown Airtime'
 
 def search_newzbin(sepis, rdict):
    def _process_results(contents, sepis):
@@ -361,62 +442,12 @@ def search_newzbin(sepis, rdict):
    search.addCallback(_process_results, sepis)
    return search
 
-def humanize(q):
-   'Converts number to a base33 format, 0-9,a-z except i,l,o (look like digits)'
-   if q < 0: raise ValueError, 'must supply a positive integer'
-   letters = '0123456789abcdefghjkmnpqrstuvwxyz'
-   converted = []
-   while q != 0:
-       q, r = divmod(q, 33)
-       converted.insert(0, letters[r])
-   return ''.join(converted)
-
-def relative_datetime(date):
-   # taken from <http://odondo.wordpress.com/2007/07/05/>, thanks!
-   if date:
-      diff = date.date() - dt.now().date()
-      
-      if diff.days == 0:
-         return "airs %s today" % date.strftime("%I:%M %p")
-      elif diff.days == -1:
-         return "aired %s yesterday" % date.strftime("%I:%M %p")
-      elif diff.days < -1:
-         return "aired on %s" % date.strftime("%m/%d/%Y")
-      elif diff.days == 1:
-         return "airs %s tomorrow" % date.strftime("%I:%M %p")
-      elif diff.days > -7:
-         return "airs %s" % date.strftime("%I:%M %p %A")
-      else:
-         return "airs on %s" % date.strftime("%m/%d/%Y")
-   else: return 'Unknown Airtime'
-
-def load():
-   with open(dbpath, 'r') as savefile:
-      return yaml.load(savefile)
-
-def cleanup(showset):
-   print "Cleaning shit up."
-   os.unlink(pidfile)
-   showset.save()
-
-def check_pid():
-   if os.path.exists(pidfile):
-      with open(pidfile) as f:
-         pid = f.read()
-      
-      try:
-         os.kill(int(pid), 0)
-      except os.error, err:
-         if err.errno == errno.ESRCH:
-            os.unlink(pidfile)
-      else:
-         return int(pid)
-
-def am_server():
-   pid = check_pid()
-   if pid is None or pid == os.getpid():
-      return True
-   else: return False
+def tvrage_info(show_name, episode):
+   episode = episode or ''
+   u = urlencode({'show': show_name, 'ep': episode})
+   info = getPage("http://tvrage.com/quickinfo.php?%s" % u)
+   info.addCallback(parse_tvrage, show_name)
+   return info
 
 def main():
    if not am_server():
@@ -445,7 +476,7 @@ def main():
       print showset.status(bool(options.verbose))
       
    elif am_server():
-      atexit.register(cleanup, showset)
+      atexit.register(at_exit, showset)
       
       tasks['tvrage'] = task.LoopingCall(showset.refresh, config['sets'])
       tasks['newzbin'] = task.LoopingCall(showset.look_on_newzbin, True)
@@ -456,6 +487,13 @@ def main():
       reactor.run()
 
 if __name__ == '__main__':
+   try:
+      with open(configpath, 'r') as configuration:
+         config = yaml.load(configuration)
+   except IOError:
+      print 'You need to set up a config file first. See the docs.'
+      sys.exit()
+   
    parser = OptionParser()
    parser.add_option('-u', '--update', action='store_true', dest='updatedb',
                      help='update show information from TVRage.')
