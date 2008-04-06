62cb2e00ec8c11d0528bcdca9ae4c397b140e3b0
Aaron Gyes <floam@sh.nu>
2008-04-06 01:36:44 -0700

    Add a readme and add make release.sh name the tarball correctly.

--- release.sh
+++ release.sh
@@ -4,12 +4,13 @@ mkdir release
 cp -X floamtv.py release
 cp -X floamtvconfig2 release
 cp -X LICENSE release
+cp -X README release
 
 sed -e s/"internal"/`bzr revno`/ floamtv.py > release/floamtv.py
 
 cd release
-tar cvvf ../release.tar *
-gzip ../release.tar
+tar cvvf ../floamtv-`bzr revno`.tar *
+gzip ../floamtv*.tar
 cd ..
 
 rm -r release
