551ad23ca9507500cb82c1662579defd44a172f2
Aaron Gyes <floam@sh.nu>
2008-04-08 20:53:21 -0700

    Smarten release.sh.

--- release.sh
+++ release.sh
@@ -6,11 +6,12 @@ mkdir ${NAME}
 cp -X floamtv.py ${NAME}
 cp -X floamtvconfig2 ${NAME}
 cp -X LICENSE ${NAME}
-cp -X ~/Sites/floamshnu/pages/stuff/floamtv.txt ${NAME}/README
+tail -n +4 ~/Sites/floamshnu/pages/stuff/floamtv.txt > ${NAME}/README
 
 sed -e s/"internal"/`bzr revno`/ floamtv.py > ${NAME}/floamtv.py
 
 tar cvvf ${NAME}.tar ${NAME}
 gzip ${NAME}.tar
+mv ${NAME}.tar.gz ~/Sites/floamshnu/static
 
 rm -r ${NAME}
